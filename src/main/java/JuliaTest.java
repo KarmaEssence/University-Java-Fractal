b827144dad52797bf310b4864d70db4d59de1c92
Akibo WorldBorder <kevin.quach360@gmail.com>
2021-11-23 13:57:10 +0100

    update

--- src/main/java/JuliaTest.java
+++ src/main/java/JuliaTest.java
@@ -3,16 +3,16 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 public class JuliaTest {
-	private double coodX1 = -1, coodX2 = 1, coodY1 = -1.2, coodY2 = 1.2;
-	private int tailleImageMax = 100;
-	private int iteration_max = 150;
+	private double coodX1 = -2.1, coodX2 = 0.6, coodY1 = -1.2, coodY2 = 1.2;
+	private int tailleImageMax = 300;
+	private int iteration_max = 500;
 	private double tailleImageX = (coodX2-coodX1)*tailleImageMax;
 	private double tailleImageY = (coodY2-coodY1)*tailleImageMax;
 
 	public JuliaTest(){}
 	
 	public void testJulia(double constante1, double constante2){
-		var img = new BufferedImage((int)tailleImageX, (int)tailleImageY, BufferedImage.TYPE_INT_RGB);
+		var img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
 		int r = 64;
 		int g = 64;
 		int b = 208;
@@ -21,17 +21,16 @@ public class JuliaTest {
 			for (int j = 0; j < tailleImageY; j++){
 				double compteur = 0;
 				double tailleImageConstante1 = i/tailleImageMax + coodX1;
-				double tailleImageConstante2 = j/tailleImageMax + coodX2;
+				double tailleImageConstante2 = j/tailleImageMax + coodY1;
 				do{
 					double tmp = tailleImageConstante1;
 					tailleImageConstante1 = tailleImageConstante1*tailleImageConstante1 - tailleImageConstante2*tailleImageConstante2 + constante1;
 					tailleImageConstante2 = 2*tailleImageConstante2*tmp + constante2;
 					compteur=compteur+1;
-				}while((tailleImageConstante1*tailleImageConstante1 + tailleImageConstante2*tailleImageConstante2) < 4 && compteur < iteration_max);
+				}while(tailleImageConstante1*tailleImageConstante1 + tailleImageConstante2*tailleImageConstante2 < 4 && compteur < iteration_max);
 				if(compteur == iteration_max){
 					img.setRGB(i,j, col);
 				}
-				else img.setRGB(i, j, b);
 			}
 		}
 		File f = new File("./ABCDE.png");
