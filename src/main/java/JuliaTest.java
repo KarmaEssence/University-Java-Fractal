aacfa7bd18b9345b29dc6e7af2c6aa7576008db8
Akibo WorldBorder <kevin.quach360@gmail.com>
2021-11-23 13:03:14 +0100

    update

--- src/main/java/JuliaTest.java
+++ src/main/java/JuliaTest.java
@@ -3,43 +3,43 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 public class JuliaTest {
-	private int coodX1, coodX2, coodY1, coodY2;
+	private double coodX1 = -1, coodX2 = 1, coodY1 = -1.2, coodY2 = 1.2;
 	private int tailleImageMax = 100;
-	private int iteration_max = 100;
-	private int tailleImageX = (coodX2-coodX1)*tailleImageMax;
-	private int tailleImageY = (coodY2-coodY1)*tailleImageMax;
+	private int iteration_max = 150;
+	private double tailleImageX = (coodX2-coodX1)*tailleImageMax;
+	private double tailleImageY = (coodY2-coodY1)*tailleImageMax;
 
 	public JuliaTest(){}
 	
-	public void testJulia(int constante1, int constante2){
-		int compteur = 0;
-		var img = new BufferedImage(tailleImageX, tailleImageY, BufferedImage.TYPE_INT_RGB);	
+	public void testJulia(double constante1, double constante2){
+		var img = new BufferedImage((int)tailleImageX, (int)tailleImageY, BufferedImage.TYPE_INT_RGB);
 		int r = 64;
 		int g = 64;
 		int b = 208;
 		int col = (r<<16) | (g<<8) | b;
 		for (int i = 0; i < tailleImageX; i++){
 			for (int j = 0; j < tailleImageY; j++){
-				int tailleImageConstante1 = i/tailleImageX + coodX1;
-				int tailleImageConstante2 = j/tailleImageY + coodX2;
-				if((tailleImageConstante1*tailleImageConstante1 + tailleImageConstante2*tailleImageConstante2) < 4 && compteur < iteration_max){
-					if(compteur == iteration_max){
-						img.setRGB(i,j, col);
-					}
-					int tmp = tailleImageConstante1;
+				double compteur = 0;
+				double tailleImageConstante1 = i/tailleImageMax + coodX1;
+				double tailleImageConstante2 = j/tailleImageMax + coodX2;
+				do{
+					double tmp = tailleImageConstante1;
 					tailleImageConstante1 = tailleImageConstante1*tailleImageConstante1 - tailleImageConstante2*tailleImageConstante2 + constante1;
 					tailleImageConstante2 = 2*tailleImageConstante2*tmp + constante2;
-					compteur++;
+					compteur=compteur+1;
+				}while((tailleImageConstante1*tailleImageConstante1 + tailleImageConstante2*tailleImageConstante2) < 4 && compteur < iteration_max);
+				if(compteur == iteration_max){
+					img.setRGB(i,j, col);
 				}
+				else img.setRGB(i, j, b);
 			}
 		}
-		File f = new File("Test.pnj");
+		File f = new File("./ABCDE.png");
 
 		try{
 			ImageIO.write(img, "PNG", f);
 		}catch (IOException e){
 			e.printStackTrace();
 		}
-
 	}
 }
